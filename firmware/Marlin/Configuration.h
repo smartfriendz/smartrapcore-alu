e2b24130f0965a4718b8add9660351ed03f23c5d
serge vi <serge.vi@gmail.com>
2016-01-22 11:09:01 +0100

    new acceleration
    changed to default acceleration = 1000

--- firmware/Marlin/Configuration.h
+++ firmware/Marlin/Configuration.h
@@ -505,7 +505,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 70, 50}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {6000,6000,2000,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for retracts
 
 // Offset of the extruders (uncomment if using more than one and relying on firmware to position when changing).
